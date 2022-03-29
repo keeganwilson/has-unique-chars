8b90d3a346db4e89c559abef943fb020a47c3770
Keegan Wilson <keegan.wilson@gmail.com>
2022-03-29 09:49:43 -0600

    completed

--- hasUniqueChars.js
+++ hasUniqueChars.js
@@ -1,1 +1,14 @@
 // Write your code below
+const uniqueChars = str => { 
+    let chars = [];
+    for (let i = 0; i < str.length; i++) {
+        if (chars.includes(str[i]) === false) {
+            chars.push(str[i])
+        }
+    }
+    if (chars.length === str.length) {
+        return true;
+    }   else return false;
+}
+
+console.log(uniqueChars('supercalifragilistic'));
